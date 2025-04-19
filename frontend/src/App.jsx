a171a23bcbc8aecdb6a62c340a53692f499b821b
cosminbrn <cosminbaroana06@gmail.com>
2025-04-20 01:25:33 +0300

    New name for page components, smaller title, changed background colors as per Figma

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import Homepage from './components/Homepage'
 import { Routes, Route } from 'react-router-dom'
-import Page1 from './components/Backgrounds'
-import Page2 from './components/Buttons'
-import Page3 from './components/Cursors'
-import Page4 from './components/Text'
-import Page5 from './components/Forms'
+import Backgrounds from './components/Backgrounds'
+import Buttons from './components/Buttons'
+import Cursors from './components/Cursors'
+import Text from './components/Text'
+import Forms from './components/Forms'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,11 +14,11 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/page1" element={<Page1 />} />
-      <Route path="/page2" element={<Page2 />} />
-      <Route path="/page3" element={<Page3 />} />
-      <Route path="/page4" element={<Page4 />} />
-      <Route path="/page5" element={<Page5 />} />
+      <Route path="/backgrounds" element={<Backgrounds />} />
+      <Route path="/buttons" element={<Buttons />} />
+      <Route path="/cursors" element={<Cursors />} />
+      <Route path="/text" element={<Text />} />
+      <Route path="/forms" element={<Forms />} />
     </Routes>
   )
 }
