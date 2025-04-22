3ba788a95665303b65608f6d206804ed7562a997
cosminbrn <cosminbaroana06@gmail.com>
2025-04-22 19:31:56 +0300

    i did the IT logo as per the attached youtube video

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,11 +1,16 @@
 import './Homepage.css'
-import title from '../assets/title.png'
+import title from '../assets/title_new.png'
 
 function Homepage() {
   return (
     <div className="homepage">
-      <div className="title">
+      <div className="title" style={{ display: 'flex', alignItems: 'center' }}>
         <img src={title} alt="Title" />
+        <div className="center">
+          <h1>
+            IT
+          </h1>
+        </div>
       </div>
     
       <div className="welcome">
