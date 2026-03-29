59144aca868ce96965a248d29358818fc9145389
cosminbrn <cosminbaroana06@gmail.com>
2026-03-29 17:56:08 +0300

    Add wave circle border

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -45,15 +45,25 @@ function Homepage() {
   const northRectangleRef = useRef(null);
   const eastRectangleRef = useRef(null);
   const southRectangleRef = useRef(null);
+  const waveBorderRef = useRef(null);
 
   const loadingDuration = 1;
   const startingDuration = 1.5;
+  const fadeDuration = 0.5;
   const movement = 600;
 
   useGSAP(() => {
 
     // aici incepe loading-ul simulat
     let tl = gsap.timeline();
+    tl.to(waveBorderRef.current, {
+      rotation: 360,
+      duration: loadingDuration,
+      ease: "linear",
+      transformOrigin: "center",
+      repeat: 1,
+    }, "loading")
+
     tl.to(innerRef.current, {
       rotation: 360,
       duration: loadingDuration,
@@ -64,8 +74,19 @@ function Homepage() {
 
     // aici incepe deschiderea
     tl.to(innerRef.current, {
+      ease: "linear",
+      transformOrigin: "center",
       autoAlpha: 0,
-      duration: startingDuration,
+      rotation: "+=360",
+      duration: fadeDuration,
+    }, "starting")
+
+    tl.to(waveBorderRef.current, {
+      rotation: "+=360",
+      ease: "linear",
+      transformOrigin: "center",
+      autoAlpha: 0,
+      duration: fadeDuration,
     }, "starting")
 
     tl.to(westRectangleRef.current, {
@@ -116,15 +137,29 @@ function Homepage() {
       <div id="preloader" className="preloader-wrapper">
   
         <svg viewBox="0 0 100 100" className="preloader-svg">
+
+          <defs>
+            <linearGradient id="cyber-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+              <stop offset="0%" stopColor="#08D0FE" /> {/* culori gradient */}
+              <stop offset="100%" stopColor="#FF007B" />
+            </linearGradient>
+          </defs>
           
-          <polygon ref={northRectangleRef} className="retracting-shape shape-top" points="50,50 -2000,-2000 2100,-2000" />
-          <polygon ref={eastRectangleRef} className="retracting-shape shape-right" points="50,50 2100,-2000 2100,2100" />
-          <polygon ref={southRectangleRef} className="retracting-shape shape-bottom" points="50,50 2100,2100 -2000,2100" />
-          <polygon ref={westRectangleRef} className="retracting-shape shape-left" points="50,50 -2000,2100 -2000,-2000" />
+          <polygon fill="url(#cyber-gradient)" ref={northRectangleRef} className="retracting-shape shape-top" points="50,50 -2000,-2000 2100,-2000" />
+          <polygon fill="url(#cyber-gradient)" ref={eastRectangleRef} className="retracting-shape shape-right" points="50,50 2100,-2000 2100,2100" />
+          <polygon fill="url(#cyber-gradient)" ref={southRectangleRef} className="retracting-shape shape-bottom" points="50,50 2100,2100 -2000,2100" />
+          <polygon fill="url(#cyber-gradient)" ref={westRectangleRef} className="retracting-shape shape-left" points="50,50 -2000,2100 -2000,-2000" />
+          <path ref={waveBorderRef} className={waveBorderRef} d="M19.9512 59.1602C13.3074 54.8607 13.3074 45.1393 19.9512 40.8398C20.3129 40.6057 20.4607 40.1501 20.3057 39.748C17.4581 32.3647 23.1722 24.5009 31.0742 24.9277C31.5043 24.9509 31.8919 24.6691 32.0029 24.2529C34.0389 16.6057 43.2837 13.6018 49.4258 18.5918C49.7602 18.8634 50.2398 18.8634 50.5742 18.5918C56.7163 13.6018 65.9611 16.6057 67.9971 24.2529C68.1081 24.6691 68.4957 24.9509 68.9258 24.9277C76.8278 24.5009 82.5419 32.3647 79.6943 39.748C79.5393 40.1501 79.6871 40.6057 80.0488 40.8398C86.6926 45.1393 86.6926 54.8607 80.0488 59.1602C79.6871 59.3943 79.5393 59.8499 79.6943 60.252C82.5419 67.6353 76.8278 75.4991 68.9258 75.0723C68.4957 75.0491 68.1081 75.3309 67.9971 75.7471C65.9611 83.3943 56.7163 86.3982 50.5742 81.4082C50.2398 81.1366 49.7602 81.1366 49.4258 81.4082C43.2837 86.3982 34.0389 83.3943 32.0029 75.7471C31.8919 75.3309 31.5043 75.0491 31.0742 75.0723C23.1722 75.4991 17.4581 67.6353 20.3057 60.252C20.4607 59.8499 20.3129 59.3943 19.9512 59.1602Z" stroke="black" stroke-opacity="0.8" stroke-width="10"/>
 
           <g id="center-circle" ref={innerRef}>
-            <circle cx="50" cy="50" r="15" className="center-bg" />
-            <circle cx="50" cy="50" r="3" className="center-dot" />
+
+
+            <circle 
+              cx="50" 
+              cy="50" 
+              r="25" 
+              className="center-bg" 
+            />
           </g>
 
         </svg>
