1dcb9926e602f5417df8ee9e79ede613baeaed07
cosminbrn <cosminbaroana06@gmail.com>
2026-04-06 13:27:57 +0300

    feat: improved animation, refactored it into its own seperate folder

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,11 +1,8 @@
 import './Homepage.scss'
 import title from '../assets/title_new.png'
-import Nav from "./Nav";
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import Carousel from './Carousel/Carousel'
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-import { CustomEase } from 'gsap/all';
+import LoadingAnimation from './LoadingAnimation/LoadingAnimation';
 
 function Homepage() {
   const [isMobile, setIsMobile] = useState(
@@ -23,9 +20,9 @@ function Homepage() {
     const mq3 = window.matchMedia(`(min-width: 701px)`);
 
     // handler when media query status changes
-    const handler = (e) => setIsMobile(e.matches);
+    const handler = (e) => setIsTablet(e.matches);
     const handler2 = (e) => setIsMobile(e.matches);
-    const handler3 = (e) => setIsMobile(e.matches);
+    const handler3 = (e) => setIsDesktop(e.matches);
 
     // set the initial value
     setIsTablet(mq.matches);
@@ -38,84 +35,12 @@ function Homepage() {
     mq3.addEventListener('change', handler3);
 
     // cleanup
-    return () => mq.removeEventListener('change', handler);
-  });
-
-  const innerRef = useRef(null);
-  const westRectangleRef = useRef(null);
-  const northRectangleRef = useRef(null);
-  const eastRectangleRef = useRef(null);
-  const southRectangleRef = useRef(null);
-  const waveBorderRef = useRef(null);
-
-  const loadingDuration = 5;
-  const startingDuration = 2;
-  const fadeDuration = 0.75;
-  const movement = 560;
-  const strokeWidth = 2;
-  const strokeColor = "#000000";
-  const firstColor = "hsl(220, 100%, 25%)";
-  const secondColor = "hsl(318, 90%, 45%)";
-  const gradientSpeed = 4;
-  const scaleFactor = 2;
-  useGSAP(() => {
-  const middleRotation = gsap.to(waveBorderRef.current, {
-    rotation: 5000,
-    duration: loadingDuration + startingDuration + 2,
-    ease: "power4.out",
-    transformOrigin: "center",
-  });
-
-  let tl = gsap.timeline();
-
-  tl.to(waveBorderRef.current, {
-    duration: loadingDuration,
-  }, "loading")
-
-  tl.to(
-    [
-      northRectangleRef.current,
-      eastRectangleRef.current,
-      southRectangleRef.current,
-      westRectangleRef.current
-    ],
-    {
-      duration: startingDuration,
-      ease: "power1.inOut",
-      stagger: 0.1,
-      x: (index) => {
-        if (index === 1) return movement;   // east
-        if (index === 3) return -movement;  // west
-        return 0;
-      },
-      y: (index) => {
-        if (index === 0) return -movement;  // north
-        if (index === 2) return movement;   // south
-        return 0;
-      },
-    },
-    "starting"
-  )
-
-  tl.to(waveBorderRef.current, {
-    autoAlpha: 0,
-    duration: fadeDuration,
-    ease: "none"
-  }, "fade")
-
-  tl.to ([
-    northRectangleRef.current,
-    eastRectangleRef.current,
-    southRectangleRef.current,
-    westRectangleRef.current,
-    innerRef.current,
-    '.preloader-wrapper'
-  ], {
-    autoAlpha: 0,
-    duration: fadeDuration,
-    ease: "none"
-  }, "fade")
-});
+    return () => {
+      mq.removeEventListener('change', handler);
+      mq2.removeEventListener('change', handler2);
+      mq3.removeEventListener('change', handler3);
+    };
+  }, []);
 
   return (
     <>
@@ -137,58 +62,7 @@ function Homepage() {
           <p>Pick a category suitable for your project!</p>
         </div>
       </div>
-      <div id="preloader" className="preloader-wrapper">
-  
-        <svg viewBox="0 0 100 100" className="preloader-svg">
-
-          <defs>
-            <linearGradient 
-              id="cyber-gradient" 
-              gradientUnits="userSpaceOnUse" 
-              x1={movement} y1={-movement} 
-              x2={-movement} y2={movement}
-            >
-              <stop offset="0%" stopColor={firstColor}>
-                <animate attributeName="stop-color" values={`${firstColor}; ${secondColor}; ${firstColor}`} dur={gradientSpeed} repeatCount="indefinite" />
-              </stop>
-              
-              <stop offset="100%" stopColor={secondColor}>
-                <animate attributeName="stop-color" values={`${secondColor}; ${firstColor}; ${secondColor}`} dur={gradientSpeed} repeatCount="indefinite" />
-              </stop>
-            </linearGradient>
-
-            <clipPath id="circle-clip">
-              <circle cx="50" cy="50" r="25" />
-            </clipPath>
-          </defs>
-          
-          <polygon stroke={strokeColor} strokeWidth={strokeWidth} fill="url(#cyber-gradient)" ref={northRectangleRef} className="retracting-shape shape-top" points="50,50 -2000,-2000 2100,-2000" />
-          <polygon stroke={strokeColor} strokeWidth={strokeWidth} fill="url(#cyber-gradient)" ref={eastRectangleRef} className="retracting-shape shape-right" points="50,50 2100,-2000 2100,2100" />
-          <polygon stroke={strokeColor} strokeWidth={strokeWidth} fill="url(#cyber-gradient)" ref={southRectangleRef} className="retracting-shape shape-bottom" points="50,50 2100,2100 -2000,2100" />
-          <polygon stroke={strokeColor} strokeWidth={strokeWidth} fill="url(#cyber-gradient)" ref={westRectangleRef} className="retracting-shape shape-left" points="50,50 -2000,2100 -2000,-2000" />
-        
-
-          <g id="center-unit" transform={`scale(${scaleFactor})`} style={{ transformOrigin: 'center' }}>
-            <path ref={waveBorderRef} className={waveBorderRef} d="M19.9512 59.1602C13.3074 54.8607 13.3074 45.1393 19.9512 40.8398C20.3129 40.6057 20.4607 40.1501 20.3057 39.748C17.4581 32.3647 23.1722 24.5009 31.0742 24.9277C31.5043 24.9509 31.8919 24.6691 32.0029 24.2529C34.0389 16.6057 43.2837 13.6018 49.4258 18.5918C49.7602 18.8634 50.2398 18.8634 50.5742 18.5918C56.7163 13.6018 65.9611 16.6057 67.9971 24.2529C68.1081 24.6691 68.4957 24.9509 68.9258 24.9277C76.8278 24.5009 82.5419 32.3647 79.6943 39.748C79.5393 40.1501 79.6871 40.6057 80.0488 40.8398C86.6926 45.1393 86.6926 54.8607 80.0488 59.1602C79.6871 59.3943 79.5393 59.8499 79.6943 60.252C82.5419 67.6353 76.8278 75.4991 68.9258 75.0723C68.4957 75.0491 68.1081 75.3309 67.9971 75.7471C65.9611 83.3943 56.7163 86.3982 50.5742 81.4082C50.2398 81.1366 49.7602 81.1366 49.4258 81.4082C43.2837 86.3982 34.0389 83.3943 32.0029 75.7471C31.8919 75.3309 31.5043 75.0491 31.0742 75.0723C23.1722 75.4991 17.4581 67.6353 20.3057 60.252C20.4607 59.8499 20.3129 59.3943 19.9512 59.1602Z" stroke="black" stroke-opacity="0.8" stroke-width="10"/>
-
-            <g id="center-circle" ref={innerRef}>
-              <text 
-                x="50" 
-                y="48"
-                fill="url(#cyber-gradient)" 
-                fontSize="32"
-                fontWeight="700"
-                textAnchor="middle"
-                fontFamily="system-ui, Avenir, Helvetica, Arial, sans-serif"
-                dominantBaseline="central"
-              >
-                IT
-              </text>
-            </g>
-          </g>
-        </svg>
-
-      </div>
+      <LoadingAnimation />
     </>
   )
 }
