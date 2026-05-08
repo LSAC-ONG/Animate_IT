4e6222ab435d7bdd29cf7959b5ff79475a6c674d
Ana Miron <anamiron05@gmail.com>
2026-05-08 23:39:16 +0300

    readme + pseudo-db

---
+++ frontend/src/AnimationsLibrary.jsx
@@ -0,0 +1,149 @@
+// --- BACKGROUNDS ---
+import FloatingBallsJsx from "./components/Backgrounds/BackgroundAnimations/FloatingBalls/FloatingBalls.jsx?raw";
+import FloatingBallsCss from "./components/Backgrounds/BackgroundAnimations/FloatingBalls/FloatingBalls.css?raw";
+import FloatingBalls from "./components/Backgrounds/BackgroundAnimations/FloatingBalls/FloatingBalls.jsx";
+import ShootingStarsJsx from "./components/Backgrounds/BackgroundAnimations/ShootingStars/ShootingStars.jsx?raw";
+import ShootingStarsCss from "./components/Backgrounds/BackgroundAnimations/ShootingStars/ShootingStars.css?raw";
+import ShootingStars from "./components/Backgrounds/BackgroundAnimations/ShootingStars/ShootingStars.jsx";
+import WavyBackgroundJsx from "./components/Backgrounds/BackgroundAnimations/WavyBackground/WavyBackground.jsx?raw";
+import WavyBackgroundCss from "./components/Backgrounds/BackgroundAnimations/WavyBackground/WavyBackground.css?raw";
+import WavyBackground from "./components/Backgrounds/BackgroundAnimations/WavyBackground/WavyBackground.jsx";
+import EyesJsx from "./components/Backgrounds/BackgroundAnimations/Eyes/Eyes.jsx?raw";
+import EyesCss from "./components/Backgrounds/BackgroundAnimations/Eyes/Eyes.css?raw";
+import Eyes from "./components/Backgrounds/BackgroundAnimations/Eyes/Eyes.jsx";
+import SquareToOctagonJsx from "./components/Backgrounds/BackgroundAnimations/SquareToOctagon/SquareToOctagon.jsx?raw";
+import SquareToOctagonCss from "./components/Backgrounds/BackgroundAnimations/SquareToOctagon/SquareToOctagon.css?raw";
+import SquareToOctagon from "./components/Backgrounds/BackgroundAnimations/SquareToOctagon/SquareToOctagon.jsx";
+import RetroStyleJsx from "./components/Backgrounds/BackgroundAnimations/RetroStyle/RetroStyle.jsx?raw";
+import RetroStyleCss from "./components/Backgrounds/BackgroundAnimations/RetroStyle/RetroStyle.css?raw";
+import RetroStyle from "./components/Backgrounds/BackgroundAnimations/RetroStyle/RetroStyle.jsx";
+import BubbleJsx from "./components/Backgrounds/BackgroundAnimations/Bubble/Bubble.jsx?raw";
+import BubbleCss from "./components/Backgrounds/BackgroundAnimations/Bubble/Bubble.css?raw";
+import Bubble from "./components/Backgrounds/BackgroundAnimations/Bubble/Bubble.jsx";
+import NoiseJsx from "./components/Backgrounds/BackgroundAnimations/Noise/Noise.jsx?raw";
+import NoiseCss from "./components/Backgrounds/BackgroundAnimations/Noise/Noise.css?raw";
+import Noise from "./components/Backgrounds/BackgroundAnimations/Noise/Noise.jsx";
+import ShapesJsx from "./components/Backgrounds/BackgroundAnimations/Shapes/Shapes.jsx?raw";
+import ShapesCss from "./components/Backgrounds/BackgroundAnimations/Shapes/Shapes.css?raw";
+import Shapes from "./components/Backgrounds/BackgroundAnimations/Shapes/Shapes.jsx";
+import FloatingDotsJsx from "./components/Backgrounds/BackgroundAnimations/FloatingDots/FloatingDots.jsx?raw";
+import FloatingDotsCss from "./components/Backgrounds/BackgroundAnimations/FloatingDots/FloatingDots.css?raw";
+import FloatingDots from "./components/Backgrounds/BackgroundAnimations/FloatingDots/FloatingDots.jsx";
+
+// --- BUTTONS ---
+import SnakeEdgeJsx from "./components/Buttons/ButtonAnimations/SnakeEdge/SnakeEdge.jsx?raw";
+import SnakeEdgeScss from "./components/Buttons/ButtonAnimations/SnakeEdge/SnakeEdge.scss?raw";
+import SnakeEdge from "./components/Buttons/ButtonAnimations/SnakeEdge/SnakeEdge.jsx";
+import CursorAwareJsx from "./components/Buttons/ButtonAnimations/CursorAware/CursorAware.jsx?raw";
+import CursorAwareScss from "./components/Buttons/ButtonAnimations/CursorAware/CursorAware.scss?raw";
+import CursorAware from "./components/Buttons/ButtonAnimations/CursorAware/CursorAware.jsx";
+import ThinLineJsx from "./components/Buttons/ButtonAnimations/ThinLine/ThinLine.jsx?raw";
+import ThinLineScss from "./components/Buttons/ButtonAnimations/ThinLine/ThinLine.scss?raw";
+import ThinLine from "./components/Buttons/ButtonAnimations/ThinLine/ThinLine.jsx";
+import ZoneInJsx from "./components/Buttons/ButtonAnimations/ZoneIn/ZoneIn.jsx?raw";
+import ZoneInScss from "./components/Buttons/ButtonAnimations/ZoneIn/ZoneIn.scss?raw";
+import ZoneIn from "./components/Buttons/ButtonAnimations/ZoneIn/ZoneIn.jsx";
+import LoadCheckJsx from "./components/Buttons/ButtonAnimations/LoadCheck/LoadCheck.jsx?raw";
+import LoadCheckScss from "./components/Buttons/ButtonAnimations/LoadCheck/LoadCheck.scss?raw";
+import LoadCheck from "./components/Buttons/ButtonAnimations/LoadCheck/LoadCheck.jsx";
+
+// --- CURSORS ---
+import DotTrailJsx from "./components/Cursors/CursorAnimations/DotTrail/DotTrail.jsx?raw";
+import DotTrailScss from "./components/Cursors/CursorAnimations/DotTrail/DotTrail.scss?raw";
+import DotTrail from "./components/Cursors/CursorAnimations/DotTrail/DotTrail.jsx";
+import ColorCirclesJsx from "./components/Cursors/CursorAnimations/ColorCircles/ColorCircles.jsx?raw";
+import ColorCirclesScss from "./components/Cursors/CursorAnimations/ColorCircles/ColorCircles.scss?raw";
+import ColorCircles from "./components/Cursors/CursorAnimations/ColorCircles/ColorCircles.jsx";
+import RainbowAuraJsx from "./components/Cursors/CursorAnimations/RainbowAura/RainbowAura.jsx?raw";
+import RainbowAuraScss from "./components/Cursors/CursorAnimations/RainbowAura/RainbowAura.scss?raw";
+import RainbowAura from "./components/Cursors/CursorAnimations/RainbowAura/RainbowAura.jsx";
+import TrailingDotsJsx from "./components/Cursors/CursorAnimations/TrailingDots/TrailingDots.jsx?raw";
+import TrailingDotsScss from "./components/Cursors/CursorAnimations/TrailingDots/TrailingDots.scss?raw";
+import TrailingDots from "./components/Cursors/CursorAnimations/TrailingDots/TrailingDots.jsx";
+import RainingSquaresJsx from "./components/Cursors/CursorAnimations/RainingSquares/RainingSquares.jsx?raw";
+import RainingSquaresScss from "./components/Cursors/CursorAnimations/RainingSquares/RainingSquares.scss?raw";
+import RainingSquares from "./components/Cursors/CursorAnimations/RainingSquares/RainingSquares.jsx";
+
+// --- FORMS ---
+import WavyLoginJsx from "./components/Forms/FormsAnimations/WavyLogin/WavyLogin.jsx?raw";
+import WavyLoginCss from "./components/Forms/FormsAnimations/WavyLogin/WavyLogin.css?raw";
+import WavyLogin from "./components/Forms/FormsAnimations/WavyLogin/WavyLogin.jsx";
+import SimpleRegJsx from "./components/Forms/FormsAnimations/SimpleRegistrationForm/SimpleRegistrationForm.jsx?raw";
+import SimpleRegCss from "./components/Forms/FormsAnimations/SimpleRegistrationForm/SimpleRegistrationForm.css?raw";
+import SimpleRegistrationForm from "./components/Forms/FormsAnimations/SimpleRegistrationForm/SimpleRegistrationForm.jsx";
+import BlockyFormJsx from "./components/Forms/FormsAnimations/BlockyForm/BlockyForm.jsx?raw";
+import BlockyFormCss from "./components/Forms/FormsAnimations/BlockyForm/BlockyForm.css?raw";
+import BlockyForm from "./components/Forms/FormsAnimations/BlockyForm/BlockyForm.jsx";
+
+// --- TEXT ---
+import DancingShadowJsx from "./components/Text/TextAnimations/DancingShadow/DancingShadow.jsx?raw";
+import DancingShadowCss from "./components/Text/TextAnimations/DancingShadow/DancingShadow.css?raw";
+import DancingShadow from "./components/Text/TextAnimations/DancingShadow/DancingShadow.jsx";
+import MatrixGlitchJsx from "./components/Text/TextAnimations/MatrixGlitch/MatrixGlitch.jsx?raw";
+import MatrixGlitchCss from "./components/Text/TextAnimations/MatrixGlitch/MatrixGlitch.css?raw";
+import MatrixGlitch from "./components/Text/TextAnimations/MatrixGlitch/MatrixGlitch.jsx";
+import PulseTextJsx from "./components/Text/TextAnimations/PulseText/PulseText.jsx?raw";
+import PulseTextCss from "./components/Text/TextAnimations/PulseText/PulseText.css?raw";
+import PulseText from "./components/Text/TextAnimations/PulseText/PulseText.jsx";
+import GlitchEffectJsx from "./components/Text/TextAnimations/GlitchEffect/GlitchEffect.jsx?raw";
+import GlitchEffectCss from "./components/Text/TextAnimations/GlitchEffect/GlitchEffect.css?raw";
+import GlitchEffect from "./components/Text/TextAnimations/GlitchEffect/GlitchEffect.jsx";
+import BorderRevolveJsx from "./components/Text/TextAnimations/BorderRevolve/BorderRevolve.jsx?raw";
+import BorderRevolveCss from "./components/Text/TextAnimations/BorderRevolve/BorderRevolve.css?raw";
+import BorderRevolve from "./components/Text/TextAnimations/BorderRevolve/BorderRevolve.jsx";
+import LiquidFillJsx from "./components/Text/TextAnimations/LiquidFill/LiquidFill.jsx?raw";
+import LiquidFillCss from "./components/Text/TextAnimations/LiquidFill/LiquidFill.css?raw";
+import LiquidFill from "./components/Text/TextAnimations/LiquidFill/LiquidFill.jsx";
+import GooeyTextJsx from "./components/Text/TextAnimations/GooeyText/GooeyText.jsx?raw";
+import GooeyTextCss from "./components/Text/TextAnimations/GooeyText/GooeyText.css?raw";
+import GooeyText from "./components/Text/TextAnimations/GooeyText/GooeyText.jsx";
+
+export const animationsLibrary = [
+// BACKGROUNDS
+{ name: "Floating Balls", category: "Backgrounds", component: FloatingBalls, jsxCode: FloatingBallsJsx, cssCode: FloatingBallsCss },
+{ name: "Shooting Stars", category: "Backgrounds", component: ShootingStars, jsxCode: ShootingStarsJsx, cssCode: ShootingStarsCss },
+{ name: "Wavy Background", category: "Backgrounds", component: WavyBackground, jsxCode: WavyBackgroundJsx, cssCode: WavyBackgroundCss },
+{ name: "Eyes", category: "Backgrounds", component: Eyes, jsxCode: EyesJsx, cssCode: EyesCss },
+{ name: "Square To Octagon", category: "Backgrounds", component: SquareToOctagon, jsxCode: SquareToOctagonJsx, cssCode: SquareToOctagonCss },
+{ name: "Retro Style", category: "Backgrounds", component: RetroStyle, jsxCode: RetroStyleJsx, cssCode: RetroStyleCss },
+{ name: "Bubble", category: "Backgrounds", component: Bubble, jsxCode: BubbleJsx, cssCode: BubbleCss },
+{ name: "Noise", category: "Backgrounds", component: Noise, jsxCode: NoiseJsx, cssCode: NoiseCss },
+{ name: "Shapes", category: "Backgrounds", component: Shapes, jsxCode: ShapesJsx, cssCode: ShapesCss },
+{ name: "Floating Dots", category: "Backgrounds", component: FloatingDots, jsxCode: FloatingDotsJsx, cssCode: FloatingDotsCss },
+
+// BUTTONS
+{ name: "Snake Edge", category: "Buttons", component: SnakeEdge, jsxCode: SnakeEdgeJsx, cssCode: SnakeEdgeScss },
+{ name: "Cursor Aware", category: "Buttons", component: CursorAware, jsxCode: CursorAwareJsx, cssCode: CursorAwareScss },
+{ name: "Thin Line", category: "Buttons", component: ThinLine, jsxCode: ThinLineJsx, cssCode: ThinLineScss },
+{ name: "Zone In", category: "Buttons", component: ZoneIn, jsxCode: ZoneInJsx, cssCode: ZoneInScss },
+{ name: "Load Check", category: "Buttons", component: LoadCheck, jsxCode: LoadCheckJsx, cssCode: LoadCheckScss },
+
+// CURSORS
+{ name: "Dot Trail", category: "Cursors", component: DotTrail, jsxCode: DotTrailJsx, cssCode: DotTrailScss },
+{ name: "Color Circles", category: "Cursors", component: ColorCircles, jsxCode: ColorCirclesJsx, cssCode: ColorCirclesScss },
+{ name: "Rainbow Aura", category: "Cursors", component: RainbowAura, jsxCode: RainbowAuraJsx, cssCode: RainbowAuraScss },
+{ name: "Trailing Dots", category: "Cursors", component: TrailingDots, jsxCode: TrailingDotsJsx, cssCode: TrailingDotsScss },
+{ name: "Raining Squares", category: "Cursors", component: RainingSquares, jsxCode: RainingSquaresJsx, cssCode: RainingSquaresScss },
+
+// FORMS
+{ name: "Wavy Login", category: "Forms", component: WavyLogin, jsxCode: WavyLoginJsx, cssCode: WavyLoginCss },
+{ name: "Simple Registration", category: "Forms", component: SimpleRegistrationForm, jsxCode: SimpleRegJsx, cssCode: SimpleRegCss },
+{ name: "Blocky Form", category: "Forms", component: BlockyForm, jsxCode: BlockyFormJsx, cssCode: BlockyFormCss },
+
+// TEXT
+{ name: "Dancing Shadow", category: "Text", component: DancingShadow, jsxCode: DancingShadowJsx, cssCode: DancingShadowCss },
+{ name: "Matrix Glitch", category: "Text", component: MatrixGlitch, jsxCode: MatrixGlitchJsx, cssCode: MatrixGlitchCss },
+{ name: "Pulse Text", category: "Text", component: PulseText, jsxCode: PulseTextJsx, cssCode: PulseTextCss },
+{ name: "Glitch Effect", category: "Text", component: GlitchEffect, jsxCode: GlitchEffectJsx, cssCode: GlitchEffectCss },
+{ name: "Border Revolve", category: "Text", component: BorderRevolve, jsxCode: BorderRevolveJsx, cssCode: BorderRevolveCss },
+{ name: "Liquid Fill", category: "Text", component: LiquidFill, jsxCode: LiquidFillJsx, cssCode: LiquidFillCss },
+{ name: "Gooey Text", category: "Text", component: GooeyText, jsxCode: GooeyTextJsx, cssCode: GooeyTextCss }
+];
+
+export const getAnimationsByCategory = (category) => {
+return animationsLibrary.filter(anim => anim.category.toLowerCase() === category.toLowerCase());
+};
+
+export const getAnimationByName = (name) => {
+return animationsLibrary.find(anim => anim.name.toLowerCase() === name.toLowerCase());
+};
